2a298901c7166912c34f5a6645a1cd7e86d96699
Gabriel7ven <gabriel.oficial@yahoo.com>
2026-01-05 17:22:06 -0300

    diagram and code

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// class Student extends Person {
-//   #year;
-
-//   constructor(name, year) {
-//     super(name);
-//     this.#year = year;
-//   }
-
-//   introduceSelf() {
-//     console.log(`Hi! I'm ${this.name}, and I'm in year ${this.#year}.`);
-//   }
-
-//   canStudyArchery() {
-//     return this.#year > 1;
-//   }
-// }
-
-class Calendar {
-    
-    construtor() {
-        this.presentMonth = new Month();
-        this.presentYear = new Year();
-        this.months_of_the_year = [];
-    }
-}
 class Year {
     #number
 
@@ -43,6 +18,11 @@ class Month {
         this.#monthIndex = monthIndex;
         this.days = [];
     }
+
+    getMonthIndex() {
+        return this.#monthIndex;
+    }
+
     getNumberOfDays() {
         pass
     }
@@ -52,14 +32,118 @@ class Month {
     }
 }
 
+
+
+
+
+
+
 class Day {
-    pass
+    #day_number;
+    #month_name;
+    #year_number;
+    #day_of_week;
+    #appointments;
+
+    constructor(day_number, month_name, year_number, day_of_week) {
+        this.#day_number = day_number;
+        this.#month_name = month_name;
+        this.#year_number = year_number;
+        this.#day_of_week = day_of_week;
+        this.#appointments = [];
+    }
+
+    getAppointments() {
+        return this.#appointments;
+    }
+
+    createAppointment() {
+        pass
+    }
 }
 
 class Appointment {
-    pass
+    #singer;
+    #inviter;
+    #date;
+
+    constructor(singer, inviter, date) {
+        this.#singer = singer;
+        this.#inviter = inviter;
+        this.#date = date;
+    }
+
+    updateDetails() {
+        pass
+    }
+
+    deleteAppointment() {
+        pass
+    }
 }
 
 class Singer {
-    pass
+    #name;
+    #number;
+    #church;
+
+    constructor(name, number, church) {
+        this.#name = name;
+        this.#number = number;
+        this.#church = church;
+    }
+
+    updateDetails() {
+        pass
+    }
+
+    deleteSinger() {
+        pass
+    }
 }
+
+
+class Calendar {
+    
+    constructor() {
+        this.todayDate = new Date();
+        this.presentMonth = new Month(this.todayDate.getMonth());
+        this.presentYear = new Year(this.todayDate.getFullYear())
+        this.months_of_the_year = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        
+    }
+
+    getTodayDate() {
+        return this.todayDate;
+    }
+
+    getPresentMonth() {
+        return this.presentMonth;
+    }
+
+    getPresentYear() {
+        return this.presentYear;
+    }
+
+    nextMonth() {
+        pass
+    }
+
+    previousMonth() {
+        pass
+    }   
+
+    goToDate(date) {
+        pass
+    }
+
+    drawCalendar() {
+        pass
+    }
+
+}
+
+
+
+let calendar = new Calendar();
+console.log(calendar.presentMonth.getMonthIndex());
