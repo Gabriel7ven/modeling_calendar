484d2fb09d7715f50ad8f117c66de839b1305274
Gabriel7ven <gabriel.oficial@yahoo.com>
2026-01-21 16:54:29 -0300

    changing fetch url to origin

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ import Calendar, { Month } from "./calendar.js"
 // const calendar = new Calendar()
 document.addEventListener("DOMContentLoaded", () => {
     const month = new Month()
-    const users = fetch('http://localhost:3000/appointments/')
+    const users = fetch('https://modeling-calendar.vercel.app//appointments/')
             .then((response) => {
                 if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //-----------------------------------------------------------------
 function changeCalendar(drawFunction) {
-       const users = fetch('http://localhost:3000/appointments/')
+       const users = fetch('https://modeling-calendar.vercel.app//appointments/')
                .then((response) => {
                    if (!response.ok) {
                    throw new Error(`HTTP error! Status: ${response.status}`);
